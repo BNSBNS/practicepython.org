62428878aafad1c623ab6fdd3262190c34252deb
BNSBNS <38092740+BNSBNS@users.noreply.github.com>
2018-04-05 14:00:15 +0800

    Add files via upload

---
+++ exercise 1.py
@@ -0,0 +1,7 @@
+name = input("give me your name: ")
+print("your name is " + name)
+
+age = int(input("what is your age? "))
+year = str((2018 - age) + 100)
+
+print("you will be 100 year old in " + year)
