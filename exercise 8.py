ef4dfec55c7dfd85f394ec554c0d4fc163512bd8
BNSBNS <38092740+BNSBNS@users.noreply.github.com>
2018-04-06 23:44:16 +0800

    Update exercise 8.py

--- exercise 8.py
+++ exercise 8.py
@@ -3,26 +3,26 @@ player2_input = input("Player 2! Select one: Rock, Scissor or Paper: " ).lower()
 
 a = "rock"
 b = "scissor" 
-c = "paper" 
+c = "paper"
 
 if player1_input == a and player2_input == b:
-    print("Player 1, you win !")
+print("Player 1, you win !")
 if player1_input == a and player2_input == c:
-    print("Player 1, you win !")
-elif player1_input == b and player2_input == c:
-    print("Player 2, you win !")
-elif player1_input == b and player2_input == a:
-    print("Player 2, you win !")
-elif player1_input == c and player2_input == b:
-    print("Player 2, you win !")
-elif player1_input == c and player2_input == a:
-    print("Player 1, you win ! ")
+print("Player 1, you win !")
 elif player1_input == a and player2_input == a:
-    print("DRAAAAAAAAAAAAAAAAW")
+print("DRAAAAAAAAAAAAAAAAW")
+elif player1_input == b and player2_input == c:
+print("Player 2, you win !")
+elif player1_input == b and player2_input == a:
+print("Player 2, you win !")
 elif player1_input == b and player2_input == b:
-    print("DRAAAAAAAAAAAAAW")
+print("DRAAAAAAAAAAAAAW")
+elif player1_input == c and player2_input == b:
+print("Player 2, you win !")
+elif player1_input == c and player2_input == a:
+print("Player 1, you win ! ")
 elif player1_input == c and player2_input == c:
-    print("DRAWWWWWWWWWWWWWWWW")
+print("DRAWWWWWWWWWWWWWWWW")
 
 else:
-    print("INVALID !!")
+print("INVALID !!")
