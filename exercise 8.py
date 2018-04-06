062c730fe1b06635ed5fd5d7e54a931e2f3a1ad3
BNSBNS <38092740+BNSBNS@users.noreply.github.com>
2018-04-06 23:38:09 +0800

    Add files via upload

---
+++ exercise 8.py
@@ -0,0 +1,26 @@
+player1_input = input("Player 1! Select one: Rock, Scissor or Paper: " ).lower()
+player2_input = input("Player 2! Select one: Rock, Scissor or Paper: " ).lower()
+
+a = "rock"
+b = "scissor" 
+c = "paper" 
+
+if player1_input == a and player2_input == b:
+    print("Player 1, you win !")
+if player1_input == a and player2_input == c:
+    print("Player 1, you win !")
+elif player1_input == b and player2_input == c:
+    print("Player 2, you win !")
+elif player1_input == b and player2_input == a:
+    print("Player 2, you win !")
+elif player1_input == c and player2_input == b:
+    print("Player 2, you win !")
+elif player1_input == c and player2_input == a:
+    print("Player 1, you win ! ")
+elif player1_input == a and player2_input == a:
+    print("DRAAAAAAAAAAAAAAAAW")
+elif player1_input == b and player2_input == b:
+    print("DRAAAAAAAAAAAAAW")
+elif player1_input == c and player2_input == c:
+    print("DRAWWWWWWWWWWWWWWWW")
+
